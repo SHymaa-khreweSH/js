ef2b5012c3f82886c7ca7af5dcc87bbdb2aa3d99
shyma'a khrewesh <128309954+SHymaa-khreweSH@users.noreply.github.com>
2025-02-15 14:44:15 +0300

    Update script.js

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -31,32 +31,43 @@ function getProductById(productId) {
   return products.find((product) => product.productId === productId);
 }
 
+/* Create a function named addProductToCart that takes in the product productId as an argument*/
 function addProductToCart(productId) {
+ /*addProductToCart should get the correct product based on the productId*/
   const product = getProductById(productId);
   
 
   if (product) {
+   /* addProductToCart should then increase the product's quantity*/
     product.quantity++;
 
     if (!cart.includes(product)) {
+     /*if the product is not already in the cart, add it to the cart*/
       cart.push(product);
     }
   }
 }
 
+/*Create a function named increaseQuantity that takes in the productId as an argument*/
 function increaseQuantity(productId) {
+ /*increaseQuantity should get the correct product based on the productId*/
   const product = getProductById(productId);
 
   if(product) {
+   /*increaseQuantity should then increase the product's quantity*/
     product.quantity++;
   }
 }
 
+/*Create a function named decreaseQuantity that takes in the productId as an argument*/
 function decreaseQuantity(productId) {
+ /*decreaseQuantity should get the correct product based on the productId*/
   const product = getProductById(productId);
 
   if (product) {
+   /*decreaseQuantity should decrease the quantity of the product*/
     product.quantity--;
+   /*if the function decreases the quantity to 0, the product is removed from the cart*/
 
     if (product.quantity === 0) {
       const index = cart.indexOf(product);
@@ -67,11 +78,15 @@ function decreaseQuantity(productId) {
   }
 }
 
+/*Create a function named removeProductFromCart that takes in the productId as an argument*/
 function removeProductFromCart(productId) {
+ /*removeProductFromCart should get the correct product based on the productId*/
   const product = getProductById(productId);
 
   if (product) {
+   /*removeProductFromCart should update the product quantity to 0*/
     product.quantity = 0;
+   /*removeProductFromCart should remove the product from the cart*/
     const index = cart.indexOf(product);
     if (index > -1) {
       cart.splice(index, 1);
@@ -79,29 +94,35 @@ function removeProductFromCart(productId) {
   }
 }
 
+/*Create a function named cartTotal that has no parameters*/
 function cartTotal() {
+ /* cartTotal should iterate through the cart to get the total cost of all products*/
   let total = 0;
   for (const product of cart) {
     total += product.price * product.quantity;
   }
+
+ /*cartTotal should return the total cost of the products in the cart*/
   return total;
 }
 
+/*Create a function called emptyCart that empties the products from the cart*/
 function emptyCart() {
   cart.length = 0;
 }
 
+/* Create a function named pay that takes in an amount as an argument*/
 function pay(amount) {
   totalPaid += amount;
 
-  const cash_returned = totalPaid - cartTotal();
+  const remainingBalance = totalPaid - cartTotal();
 
-  if (cash_returned >= 0) {
+  if (remainingBalance >= 0) {
     totalPaid = 0;
     emptyCart();
   }
 
-  return cash_returned;
+  return remainingBalance;
 }
 
 module.exports = {
